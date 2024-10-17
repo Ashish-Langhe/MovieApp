d219c402c7416f6ca483b469c72f4e786f2acc61
Ashish-Langhe <langheashish389@gmail.com>
2024-10-17 22:04:55 +0530

    initial commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Ashish Langhe on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
